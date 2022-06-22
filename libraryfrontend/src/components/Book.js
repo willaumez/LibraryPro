9bdcb0b54a1d5029ca41e8d2ebfdb2c7da84dcf0
OWANI-SANA Jency-w <owani-sana_jency-w@MacBook-Pro-de-OWANI-SANA.local>
2022-06-22 18:44:09 +0100

    ma

--- libraryfrontend/src/components/Book.js
+++ libraryfrontend/src/components/Book.js
@@ -7,7 +7,6 @@ import axios from "axios";
 import {GetCategory} from "./GetCategoryName";
 
 
-
 function Book({book}) {
 
     //const [isCate, setIsCate] = useState(false)
@@ -19,7 +18,7 @@ function Book({book}) {
     return (
         <Card className="my-3 p-2" style={{textAlign: "center", borderRadius: 30}}>
             <Link to={`/book/${book._id}`}>
-                <Card.Img src={`${book.image}`}
+                <Card.Img src={`/static${book.image}`}
                           style={{height: 480, borderRadius: '30px 30px 0px 0px'}}/>
             </Link>
             <Card.Body>
