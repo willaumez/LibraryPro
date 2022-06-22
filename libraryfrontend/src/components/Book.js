f9fb996c1d2450bba0afde8d373fe79b823bbe53
OWANI-SANA Jency-w <owani-sana_jency-w@MacBook-Pro-de-OWANI-SANA.local>
2022-06-22 18:36:57 +0100

    ma

--- libraryfrontend/src/components/Book.js
+++ libraryfrontend/src/components/Book.js
@@ -7,6 +7,8 @@ import axios from "axios";
 import {GetCategory} from "./GetCategoryName";
 
 
+const proxy = 'https://libraryprojectg2.s3.eu-north-1.amazonaws.com/media/'
+
 function Book({book}) {
 
     //const [isCate, setIsCate] = useState(false)
@@ -18,7 +20,7 @@ function Book({book}) {
     return (
         <Card className="my-3 p-2" style={{textAlign: "center", borderRadius: 30}}>
             <Link to={`/book/${book._id}`}>
-                <Card.Img src={`/static${book.image}`}
+                <Card.Img src={`${proxy}${book.image}`}
                           style={{height: 480, borderRadius: '30px 30px 0px 0px'}}/>
             </Link>
             <Card.Body>
